c7e9b1605f1442197f3fb0bf4e1fad53ea3d5dfe
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2016-08-29 00:00:59 +0200

    reset, naming, typos

--- main/main.ino
+++ main/main.ino
@@ -10,7 +10,7 @@
 #define MPU9250_REG_CONFIG           0x1A
 #define MPU9250_REG_GYRO_CONFIG      0x1B
 
-#define MPU8250_REG_GYRO_XOUT_H      0x43
+#define MPU9250_REG_GYRO_XOUT_H      0x43
 
 enum GyroMode {
   GyroMode_250dps = 0,
@@ -24,7 +24,24 @@ uint8_t i2c_read_reg(uint8_t address, uint8_t reg);
 void i2c_write_reg(uint8_t address, uint8_t reg, uint8_t value);
 void i2c_read_range(uint8_t address, uint8_t reg_base, uint8_t count, uint8_t *dest);
 
-void verify_device()
+/* state global variables, should be turned into a class instance */
+GyroMode gyro_mode = GyroMode_1000dps;
+
+/* stolen from some open source project :-) (beerware, remember to buy author a beer) 
+ * https://github.com/kriswiner/MPU-9250/blob/master/MPU9250BasicAHRS.ino */
+const float gyro_coefficients[4] = {
+  250.0/32768.0,
+  500.0/32768.0,
+  1000.0/32768.0,
+  2000.0/32768.0
+};
+
+void mpu9250_reset()
+{
+  i2c_write_reg(MPU9250_ADDRESS_MAIN, MPU9250_REG_PWR_MGMT_1, 0x80);
+}
+
+void mpu9250_verify()
 {
   byte who = i2c_read_reg(MPU9250_ADDRESS_MAIN, MPU9250_REG_WHO_AM_I);
 
@@ -39,18 +56,6 @@ void verify_device()
   }
 }
 
-/* state global variables, should be turned into a class instance */
-GyroMode gyro_mode = GyroMode_1000dps;
-
-/* stolen from some open source project :-) (beerware, remember to buy author a beer) 
- * https://github.com/kriswiner/MPU-9250/blob/master/MPU9250BasicAHRS.ino */
-const float gyro_coefficients[4] = {
-  250.0/32768.0,
-  500.0/32768.0,
-  1000.0/32768.0,
-  2000.0/32768.0
-};
-
 void setup()
 {
   Serial.begin(115200);
@@ -61,14 +66,15 @@ void setup()
   Wire.begin();
   delay(100);
 
-  // 1. verification
-  verify_device();
+  // 1. verification & reset device
+  mpu9250_verify();
+  mpu9250_reset();
 
   // 2. wake-up device, auto-select clock source
   i2c_write_reg(MPU9250_ADDRESS_MAIN, MPU9250_REG_PWR_MGMT_1, 1);
 
   // 3. set configuration register to known value
-  i2c_write_reg(MPU9250_ADDRESS_MAIN, MPU9250_REG_CONFIG, 0);
+  i2c_write_reg(MPU9250_ADDRESS_MAIN, MPU9250_REG_CONFIG, 3);
 
   // 4. configure gyro
   i2c_write_reg(MPU9250_ADDRESS_MAIN, MPU9250_REG_GYRO_CONFIG, gyro_mode << 3);
@@ -83,7 +89,7 @@ void loop()
 {
   // fetch gyro data
   uint8_t rawData[6];
-  i2c_read_range(MPU9250_ADDRESS_MAIN, MPU8250_REG_GYRO_XOUT_H, 6, &rawData[0]);
+  i2c_read_range(MPU9250_ADDRESS_MAIN, MPU9250_REG_GYRO_XOUT_H, 6, &rawData[0]);
   int16_t gyro_x = ((int16_t)rawData[0] << 8) | rawData[1];
   int16_t gyro_y = ((int16_t)rawData[2] << 8) | rawData[3];
   int16_t gyro_z = ((int16_t)rawData[4] << 8) | rawData[5];
